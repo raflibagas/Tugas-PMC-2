5941e8ba5491164bafceeea09e5442ac9f4ce2ce
raflibagas <13220023@std.stei.itb.ac.id>
2022-01-27 11:49:27 +0700

    first commit

---
+++ main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("Maaf Pak Saya belum selesai mengerjakan, nanti saya update lagi setelah saya menyelesaikan tugasnya");
+    return 0;
+}
